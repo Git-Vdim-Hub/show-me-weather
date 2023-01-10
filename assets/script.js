dd2c63d9a66a08c817651275f5ab78b81372b3ad
Crypto_Vdim <vadimrace@gmail.com>
2023-01-09 21:22:16 -0500

    added bootstrap components

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//TO-DO
+//1. complete styling the webpage
+   //review Mod 5 Third Party APIS regarding bootstrap (look at the layouts)
+   //-review all bootstrap helpers and utilities
+
+
+console.log("Hello");
