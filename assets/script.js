3a8655450f72cdcfddf53ee7ed5f7d78bc03c9d4
Crypto_Vdim <vadimrace@gmail.com>
2023-01-18 18:31:45 -0500

    search button working, current weather pulled with geolocation API and displayed on main card

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,45 @@
-//TO-DO
-//1. add dayjs to html, call day js in script for todays date.
-//2. read WX API docs
+
 var apiKey = 'e22a952731360f3f21865b6d5114ce96';
 var lon;
 var lat;
-var units;
+var units = 'imperial';
+var searchButton = document.querySelector('#wxSearch');
+var cityInput = document.querySelector('#cityInput');
 
-function getForecast(lat, lon, cardNumber) {
-    lat = lat;
-    lon = lon;
-    units = 'imperial';
+searchButton.addEventListener('click', function(event){
+  getWxLatLon(cityInput.value);
+});
+
+function getWxLatLon(cityName){
+  var requestGEO = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${units}&appid=${apiKey}`;
+  fetch(requestGEO)
+    .then(function (response) {
+      if(response.ok){
+        response.json().then(function(data){
+          //temp, wind, wxIcon, humidity, dtg
+        console.log(data);
+        lat = data.coord.lat;
+        lon = data.coord.lon;
+        var cityName = data.name;
+        var temp = data.main.temp;
+        var wind = data.wind.speed;
+        var icon = data.weather[0].icon;
+        console.log(icon);
+        var hum = data.main.humidity;
+        displayWeather(cityName, icon, temp, wind, hum);
+        getForecast(lat, lon);
+        });
+      } else{
+          alert('Error: '+ response.statusText);
+        }
+
+    })
+      .catch(function(error){
+      alert('Unable to connect to geocoding API');
+        })
+}
+
+function getForecast(lat, lon) {
     var temp1;
     var wind1;
     var wxIcon1;
@@ -28,13 +58,25 @@ function getForecast(lat, lon, cardNumber) {
     var humidity3;
     var when3;
 
+    var temp4;
+    var wind4;
+    var wxIcon4;
+    var humidity4;
+    var when4;
+
+    var temp5;
+    var wind5;
+    var wxIcon5;
+    var humidity5;
+    var when5;
+
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
 
     fetch(requestUrl)
         .then(function (response) {
           if(response.ok){
             response.json().then(function(data){
-              //get new day data at 0(tomorrow), 8 (day+1), 16 (day+2),
+              //get new day data at 0(tomorrow), 8 (day+1), 16 (day+2) 24 (day+3) 32 (day+4),
               //temp, wind, wxIcon, humidity, dtg
               //console.log(data);
               temp1 = data.list[0].main.temp;
@@ -80,13 +122,37 @@ function getForecast(lat, lon, cardNumber) {
         })
 }
 
-getForecast(43.1566, -77.6088)
-
-
 function displayWeatherIcon(appendEl, iconCode){
     var imgEl = document.createElement("img");
     imgEl.src = `http://openweathermap.org/img/wn/${iconCode}.png`
     appendEl.appendChild(imgEl);
+}
+
+function displayWeather(cityName, icon, temp, wind, hum){
+  var date = dayjs().format('ddd, D MMM YY');
+  console.log (date);
+  var weatherDisplay = document.querySelector('#day0');
+  var innerDiv = document.createElement('div');
+  innerDiv.classList.add('card-body', 'border', 'border-success');
+  var h4El = document.createElement('h4');
+  var tempP = document.createElement('p');
+  var windP = document.createElement('p');
+  var humP = document.createElement('p');
+  h4El.innerHTML = `${cityName} (${date})`
+  h4El.classList.add('card-title', 'text-start');
+  tempP.classList.add('card-text', 'text-start');
+  windP.classList.add('card-text', 'text-start');
+  humP.classList.add('card-text', 'text-start');
+  weatherDisplay.appendChild(innerDiv);
+  innerDiv.appendChild(h4El);
+  displayWeatherIcon(h4El, icon);
+  tempP.innerHTML = `Temp: ${temp}°F`
+  windP.innerHTML = `Winds: ${wind} MPH`
+  humP.innerHTML = `Humidity: ${hum} %`
+  innerDiv.appendChild(tempP);
+  innerDiv.appendChild(windP);
+  innerDiv.appendChild(humP);
+  
 }
 
 function displayDayForecast(functIcon, funcTemp, funcWind, funcHumidity, funcWhen, dayNum){
@@ -95,9 +161,13 @@ function displayDayForecast(functIcon, funcTemp, funcWind, funcHumidity, funcWhe
     wxDateP = document.createElement('h6');
     wxDateP.classList.add('card-title');
     wxIconP = document.createElement('p');
+    wxIconP.classList.add('card-text');
     wxTempP = document.createElement('p');
+    wxTempP.classList.add('card-text');
     wxWindP = document.createElement('p');
+    wxWindP.classList.add('card-text', 'text-start');
     wxHumP = document.createElement('p');
+    wxHumP.classList.add('card-text', 'text-start');
     wxDateP.innerHTML = `${dayjs(mark).format('ddd, D MMM YY')}`;
     displayWeatherIcon(wxIconP, functIcon);
     wxTempP.innerHTML = `Temp: ${funcTemp} °F`;
